169782580a6d833c55bd60d2c144eb00137c47f3
DANGKHOADN\ad <dangkhoadn163@gmail.com>
2021-06-24 22:31:42 +0700

    Test account

--- lib/main.dart
+++ lib/main.dart
@@ -78,7 +78,7 @@ class _HomeState extends State<Home> {
   int _currentIndex = 0;
   final List<Widget> _children = [
     new Tab1(),
-    PlaceholderWidget(Colors.deepOrange),
+    new Tab2(),
     PlaceholderWidget(Colors.green),
     PlaceholderWidget(Colors.amber),
     PlaceholderWidget(Colors.black)
