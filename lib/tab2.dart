0d66fc055ec1f0e7fb8838e267e0ed19920b11ec
DANGKHOADN\ad <dangkhoadn163@gmail.com>
2021-07-24 22:41:35 +0700

    Test post/put data

--- lib/tab2.dart
+++ lib/tab2.dart
@@ -1,13 +1,156 @@
-import 'package:flutter/material.dart';
+// import 'package:flutter/material.dart';
+//
+// class Tab2 extends StatelessWidget {
+//   @override
+//   Widget build(BuildContext context) {
+//     return new Center(
+//       child: new Text(
+//         "This is Tab2",
+//         style: new TextStyle(fontSize: 25),
+//       ),
+//     );
+//   }
+// }
 
-class Tab2 extends StatelessWidget {
+import 'dart:async';
+import 'dart:convert';
+
+import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
+
+Future<Album> fetchAlbum() async {
+  final response = await http.get(
+    Uri.parse('http://192.168.100.19:8000/album.json'),
+  );
+
+  if (response.statusCode == 200) {
+    // If the server did return a 200 OK response,
+    // then parse the JSON.
+    return Album.fromJson(jsonDecode(response.body));
+  } else {
+    // If the server did not return a 200 OK response,
+    // then throw an exception.
+    throw Exception('Failed to load album');
+  }
+}
+
+Future<Album> createAlbum(String title) async {
+  final response = await http.post(
+    Uri.parse('https://jsonplaceholder.typicode.com/albums'),
+    // Uri.parse('http://192.168.100.19:8000/album.json'),
+    headers: <String, String>{
+      'Content-Type': 'application/json; charset=UTF-8',
+    },
+    body: jsonEncode(<String, String>{
+      'title': title,
+    }),
+  );
+
+  if (response.statusCode == 201) {
+    // If the server did return a 201 CREATED response,
+    // then parse the JSON.
+    return Album.fromJson(jsonDecode(response.body));
+  } else {
+    // If the server did not return a 201 CREATED response,
+    // then throw an exception.
+    throw Exception('Failed to create album.');
+  }
+}
+
+Future<Album> updateAlbum(String title) async {
+  final response = await http.put(
+    Uri.parse('https://jsonplaceholder.typicode.com/albums/1'),
+    // Uri.parse('http://192.168.100.19:8000/album.json'),
+    headers: <String, String>{
+      'Content-Type': 'application/json; charset=UTF-8',
+    },
+    body: jsonEncode(<String, String>{
+      'title': title,
+    }),
+  );
+
+  if (response.statusCode == 200) {
+    // If the server did return a 200 OK response,
+    // then parse the JSON.
+    return Album.fromJson(jsonDecode(response.body));
+  } else {
+    // If the server did not return a 200 OK response,
+    // then throw an exception.
+    throw Exception('Failed to update album.');
+  }
+}
+
+class Album {
+  final int id;
+  final String title;
+
+  Album({this.id, this.title});
+
+  factory Album.fromJson(Map<String, dynamic> json) {
+    return Album(
+      id: json['id'],
+      title: json['title'],
+    );
+  }
+}
+
+class Tab2 extends StatefulWidget {
+  Tab2({Key key}) : super(key: key);
+
+  @override
+  _MyAppState createState() {
+    return _MyAppState();
+  }
+}
+
+class _MyAppState extends State<Tab2> {
+  final TextEditingController _controller = TextEditingController();
+  Future<Album> _futureAlbum;
+
   @override
   Widget build(BuildContext context) {
-    return new Center(
-      child: new Text(
-        "This is Tab2",
-        style: new TextStyle(fontSize: 25),
-      ),
+    return Scaffold(
+        body: Container(
+          alignment: Alignment.center,
+          padding: const EdgeInsets.all(8.0),
+          child: (_futureAlbum == null) ? buildColumn() : buildFutureBuilder(),
+        ),
+      );
+  }
+
+  Column buildColumn() {
+    return Column(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: <Widget>[
+        TextField(
+          controller: _controller,
+          decoration: InputDecoration(hintText: 'Enter Title'),
+        ),
+        ElevatedButton(
+          onPressed: () {
+            setState(() {
+              // _futureAlbum = createAlbum(_controller.text);
+              _futureAlbum = updateAlbum(_controller.text);
+            });
+          },
+          child: Text(' Data'),
+        ),
+      ],
+    );
+  }
+
+  FutureBuilder<Album> buildFutureBuilder() {
+    return FutureBuilder<Album>(
+      future: _futureAlbum,
+      builder: (context, snapshot) {
+        if (snapshot.hasData) {
+          return Text(snapshot.data.title);
+        } else if (snapshot.hasError) {
+          return Text('${snapshot.error}');
+        }
+
+        return CircularProgressIndicator();
+      },
     );
   }
 }
